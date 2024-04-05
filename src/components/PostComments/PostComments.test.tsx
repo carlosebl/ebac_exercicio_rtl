607add68582a87d7fb65aa05dc56a1cdb8644176
carlosebl <carlosebl@gmail.com>
2024-04-05 14:41:40 -0300

    criado teste de inserção de comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,24 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Deve adicionar dois comentários', () => {
+        render(<PostComment/>);
+
+        fireEvent.change(screen.getByTestId('ta-comentario'), {
+            target: {
+                value: 'Primeiro comentário de teste',
+            }
+        });
+        fireEvent.click(screen.getByTestId('btn-comentario'));
+    
+        fireEvent.change(screen.getByTestId('ta-comentario'), {
+            target: {
+                value: 'Segundo comentário de teste',
+            }
+        });
+        fireEvent.click(screen.getByTestId('btn-comentario'));
+
+        expect(screen.getAllByTestId('li-comentario')).toHaveLength(2);
+    });
 });
